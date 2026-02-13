08e03ad42abb229b8eae1e570800c96d81ee3544
BrunoEPaez <emmanuelnovo007@gmail.com>
2026-02-13 16:15:24 -0300

    Fix: TS types and bundle optimization

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000, // Sube el límite para que no se queje
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
-        // Esto separa las librerías grandes en archivos aparte
         manualChunks(id) {
           if (id.includes('node_modules')) {
+            // Esto separa librerías como jsPDF y html2canvas en archivos aparte
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         }
