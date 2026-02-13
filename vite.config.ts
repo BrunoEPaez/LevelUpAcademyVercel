e8eeef5dfd7385e132ffc81f25750ce02b147548
BrunoEPaez <emmanuelnovo007@gmail.com>
2026-02-13 16:10:06 -0300

    Fix: TS types and build optimization

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000, // Esto quita el aviso de los "chunks" grandes
+    chunkSizeWarningLimit: 2000, // Sube el límite para que no se queje
+    rollupOptions: {
+      output: {
+        // Esto separa las librerías grandes en archivos aparte
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 })
